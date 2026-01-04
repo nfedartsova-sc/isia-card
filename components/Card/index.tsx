cb0af888df1a524f794429adbccf757c2a579de3
Natallia Fedartsova <fnm>
2026-01-04 23:26:02 +0300

    28

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -228,7 +228,7 @@ export default function ISIACard() {
           >
             <img src="/images/logo.svg" alt="Go to main page" />
           </Link>
-          <span>Your card (looks not good)</span>
+          <span>Your card (looks better)</span>
         </div>
         {(!showLoading && isTouchDevice !== undefined) &&
           <div>
